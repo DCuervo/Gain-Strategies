a6b3d3b34287d71d0981a978f205d65aa0eca2d5
Daniel Cuervo <dcuervo@i2rcorp.com>
2015-09-02 15:20:10 -0500

    Insert Case 4 and correct logic

--- GainStrategies.m
+++ GainStrategies.m
@@ -45,7 +45,7 @@ out_dir = 'C:\Users\I2R_Admin\Documents\Work\Study\Git\RepositoryExample\Outputs
 
 % Set up output file
 gainDetailsFile = 'GainStrategies.xlsx';
-IDT = {'Time\Control Case', 'Control Rate', 'Case 1', 'Rate 1', 'Case 2', 'Rate 2', 'Case 3', 'Rate 3'};
+IDT = {'Time\Control Case', 'Control Rate', 'Case 1', 'Rate 1', 'Case 2','Rate 2', 'Case 3', 'Rate 3', 'Case 4', 'Rate 4'};
 IDI = {};
 IDD = 0;
 rowToWrite = 0;
@@ -63,50 +63,65 @@ sumCase2 = 0;
 gainCase2 = controlGain;
 sumCase3 = 0;
 gainCase3 = controlGain;
+sumCase4 = 0;
+gainCase4 = controlGain;
 
 for t = 0:controlGain:timeLength
     rowToWrite = rowToWrite + 1;
     
-    % Gain
+    % Document counts
     % Control case
     IDD(rowToWrite, 1) = t;
     IDD(rowToWrite, 2) = controlGain/0.1;
     % Case 1
     IDD(rowToWrite, 3) = sumCase1;
     IDD(rowToWrite, 4) = gainCase1/0.1;
-    sumCase1 = sumCase1 + gainCase1;
     % Case 2
     IDD(rowToWrite, 5) = sumCase2;
     IDD(rowToWrite, 6) = gainCase2/0.1;
-    sumCase2 = sumCase2 + gainCase2;
 	% Case 3
 	IDD(rowToWrite, 7) = sumCase3;
 	IDD(rowToWrite, 8) = gainCase3/0.1;
-	sumCase3 = sumCase3 + gainCase3;
+    % Case 4
+    IDD(rowToWrite, 9) = sumCase4;
+    IDD(rowToWrite, 10) = gainCase4/0.1; 
     
     % Change rates
     % Case 1
     if haveInc == 0
-        if sumCase1 >= 5
+        if single(sumCase1) >= 5
             haveInc = 1;
             sumCase1 = double(single(sumCase1) - 5);
-            gainCase1 = gainCase1 + (increaseRate*0.1);
+            gainCase1 = gainCase1 + (increaseRate*controlGain);
         end
     end
     % Case 2
     if mod(t, 1) == 0
-        if sumCase2 >= 5
-            sumCase2 = sumCase2 - 5;
-            gainCase2 = gainCase2 + (increaseRate*0.1);
+        if single(sumCase2) >= 5
+            sumCase2 = double(single(sumCase2) - 5);
+            gainCase2 = gainCase2 + (increaseRate*controlGain);
         end
     end
 	% Case 3
 	if mod(t, 1) == 0
-		if sumCase3 >= 5
-			sumCase3 = sumCase3 - 5;
+		if single(sumCase3) >= 5
+			sumCase3 = double(single(sumCase3) - 5);
 			gainCase3 = gainCase3*(1 + increaseRate);
 		end
-	end
+    end
+    % Case 4
+    if mod(t, 1) == 0
+        while single(sumCase4) >= 5
+            sumCase4 = double(single(sumCase4) - 5);
+            gainCase4 = gainCase4*(1 + increaseRate);
+        end
+    end
+    
+    % Increment cases
+    sumCase1 = sumCase1 + gainCase1;
+    sumCase2 = sumCase2 + gainCase2;
+	sumCase3 = sumCase3 + gainCase3;
+    sumCase4 = sumCase4 + gainCase4;
 end
 
 figure;
@@ -115,6 +130,7 @@ plot(IDD(:, 1), IDD(:, 1), 'k', 'LineWidth', 2);
 plot(IDD(:, 1), IDD(:, 3), 'r', 'LineWidth', 2);
 plot(IDD(:, 1), IDD(:, 5), 'b', 'LineWidth', 2);
 plot(IDD(:, 1), IDD(:, 7), 'g', 'LineWidth', 2);
+plot(IDD(:, 1), IDD(:, 9), 'm', 'LineWidth', 2);
 title('Gain Strategies: Count', 'Interpreter', 'none');
 xlabel('Time', 'FontSize', 12);
 ylabel('Count', 'FontSize', 12);
@@ -128,6 +144,7 @@ plot(IDD(:, 1), IDD(:, 2), 'k', 'LineWidth', 2);
 plot(IDD(:, 1), IDD(:, 4), 'r', 'LineWidth', 2);
 plot(IDD(:, 1), IDD(:, 6), 'b', 'LineWidth', 2);
 plot(IDD(:, 1), IDD(:, 8), 'g', 'LineWidth', 2);
+plot(IDD(:, 1), IDD(:, 10), 'm', 'LineWidth', 2);
 title('Gain Strategies: Rate', 'Interpreter', 'none');
 xlabel('Time', 'FontSize', 12);
 ylabel('Rate', 'FontSize', 12);
